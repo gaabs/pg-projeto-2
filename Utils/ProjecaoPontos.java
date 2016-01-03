422035d120e84c15c8f4b26f8187dd6d59c323bc
ehammo <ehammo@cin.ufpe.br>
2016-01-03 11:57:44 -0200

    Comentarios

--- Utils/ProjecaoPontos.java
+++ Utils/ProjecaoPontos.java
@@ -13,4 +13,26 @@ public class ProjecaoPontos extends MatrixUtil{
 				   matrizResultadoTransposto = transpor(matrizResultado, D, n); //nxD
 		return matrizResultadoTransposto;
 	}
+	
+	/*Há grande chance disto ai esta errado.
+	 * Sabendo disto é necessario q alguém traduza isso:
+	 * 
+	 * "Projetar cada ponto que a camera enxerga para as coordenadas da tela (x, y)
+
+	Coordenadas = ( d*(Xalfa/Zalfa) , d*(Yalfa/Zalfa) )
+
+	Normalizando para ficar entre -1 e 1
+	((d/Hx)*(Xalfa/Zalfa) , (d/Hy)*(Yalfa/Zalfa))
+	( 		Xd			  ,			Yd			)
+
+
+	Para mapear para a tela do computador 
+	(onde o centro do plano esta no topo esquerdo da tela)
+	Xs = (Xd+1)/2 * (ResX - 1)
+	Ys = (1-Yd)/2 * (ResY - 1)"
+	 * 
+	 * 
+	 * 
+	 * */
+	
 }
