f2cc5d9cdeb4beb4fce290ed9f1e77d26070ff00
ehammo <ehammo@cin.ufpe.br>
2016-01-14 22:14:23 -0200

    classes staticas

---
+++ entidades/Camera.java
@@ -0,0 +1,6 @@
+package entidades;
+
+public class Camera {
+	public static Point C, N, V, Vo,No,Vn,U; 
+	
+}
---
+++ entidades/Iluminacao.java
@@ -0,0 +1,13 @@
+package entidades;
+
+public class Iluminacao {
+	public static Point Pl; // - Coordenadas do ponto de luz
+	public static double ka; // - reflexao ambiental
+	public static Point Ia; // - vetor cor ambiental
+	public static double kd; // - constante difusa
+	public static Point Od; // - vetor difuso
+	public static double ks; // - coeficiente especular
+	public static Point Il; // - cor da fonte de luz
+	public static double n; // - constante de rugosidade
+}
+
